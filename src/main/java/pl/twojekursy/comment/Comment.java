8128ebb20d0e90c92f059d798f9d6cc8c7b20907
Marek Koszałka <kmarek9@gmail.com>
2023-04-20 10:54:01 +0200

    added comment

---
+++ src/main/java/pl/twojekursy/comment/Comment.java
@@ -0,0 +1,126 @@
+package pl.twojekursy.comment;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.hibernate.envers.Audited;
+import org.hibernate.envers.NotAudited;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Audited
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @NotNull
+    private LocalDateTime createdDateTime;
+
+    @LastModifiedDate
+    @NotNull
+    private LocalDateTime lastModifiedDateTime;
+
+    @NotBlank
+    @NotNull
+    @Size(max = 5000)
+    private String text;
+
+    @NotBlank
+    @NotNull
+    @Size(max = 100)
+    @NotAudited
+    private String author;
+
+    @NotNull
+    private Long postId;
+
+    public Comment(){
+
+    }
+
+    public Comment(Comment old){
+        this.id = old.id;
+        this.createdDateTime=old.createdDateTime;
+        this.lastModifiedDateTime=old.lastModifiedDateTime;
+        this.text = old.text;
+        this.author = old.author;
+        this.postId = old.postId;
+    }
+
+    public Comment(String text, String author) {
+        this.text = text;
+        this.author = author;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public String getText() {
+        return text;
+    }
+
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public LocalDateTime getCreatedDateTime() {
+        return createdDateTime;
+    }
+
+    public void setCreatedDateTime(LocalDateTime createdDateTime) {
+        this.createdDateTime = createdDateTime;
+    }
+
+    public LocalDateTime getLastModifiedDateTime() {
+        return lastModifiedDateTime;
+    }
+
+    public void setLastModifiedDateTime(LocalDateTime lastModifiedDateTime) {
+        this.lastModifiedDateTime = lastModifiedDateTime;
+    }
+
+    public Long getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Long postId) {
+        this.postId = postId;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", createdDateTime=" + createdDateTime +
+                ", lastModifiedDateTime=" + lastModifiedDateTime +
+                ", text='" + text + '\'' +
+                ", author='" + author + '\'' +
+                ", postId=" + postId +
+                '}';
+    }
+}
