dc825d17eba575d36be5db1af54cd8f2ccf23b07
Marek Koszałka <kmarek9@gmail.com>
2023-05-09 14:37:37 +0200

    Accountant added

---
+++ src/main/java/pl/twojekursy/accountant/Accountant.java
@@ -0,0 +1,45 @@
+package pl.twojekursy.accountant;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.hibernate.envers.Audited;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Audited
+@Data
+@NoArgsConstructor
+@ToString()
+@EqualsAndHashCode()
+@Builder(toBuilder = true)
+@AllArgsConstructor
+public class Accountant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Version
+    @NotNull
+    private Integer version;
+
+    @CreatedDate
+    @NotNull
+    private LocalDateTime createdDateTime;
+
+    @LastModifiedDate
+    @NotNull
+    private LocalDateTime lastModifiedDateTime;
+
+    @NotBlank
+    @NotNull
+    @Size(max = 100)
+    private String name;
+}
---
+++ src/main/java/pl/twojekursy/accountant/AccountantRepository.java
@@ -0,0 +1,6 @@
+package pl.twojekursy.accountant;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AccountantRepository extends CrudRepository<Accountant, Long> {
+}
