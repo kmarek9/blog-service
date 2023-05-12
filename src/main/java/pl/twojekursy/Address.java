8bc37b9770edfbc228ff3a18b2f2d453a1e54541
Marek Koszałka <kmarek9@gmail.com>
2023-05-12 13:13:19 +0200

    one to one, Address added

---
+++ src/main/java/pl/twojekursy/Address.java
@@ -0,0 +1,55 @@
+package pl.twojekursy;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.hibernate.envers.Audited;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import pl.twojekursy.user.User;
+
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Audited
+@Data
+@NoArgsConstructor
+@ToString(exclude = "user")
+@EqualsAndHashCode(exclude = "user")
+@Builder(toBuilder = true)
+@AllArgsConstructor
+public class Address {
+    @Id
+    private Long id;
+
+    @Version
+    @NotNull
+    private Integer version;
+
+    @CreatedDate
+    @NotNull
+    private LocalDateTime createdDateTime;
+
+    @LastModifiedDate
+    @NotNull
+    private LocalDateTime lastModifiedDateTime;
+
+    @NotBlank
+    @NotNull
+    @Size(max = 100)
+    private String street;
+
+    @NotBlank
+    @NotNull
+    @Size(max = 100)
+    private String city;
+
+    @OneToOne(optional = false , fetch = FetchType.LAZY)
+    @MapsId
+    @JoinColumn(name = "id")
+    private User user;
+}
