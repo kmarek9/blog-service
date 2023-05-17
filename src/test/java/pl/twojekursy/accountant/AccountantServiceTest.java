8c83751af571308b12c39c575df5671637afd823
Marek Koszałka <kmarek9@gmail.com>
2023-05-17 14:26:36 +0200

    tests for attachClient method - homework

--- src/test/java/pl/twojekursy/accountant/AccountantServiceTest.java
+++ src/test/java/pl/twojekursy/accountant/AccountantServiceTest.java
@@ -1,12 +1,20 @@
 package pl.twojekursy.accountant;
 
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import org.mockito.*;
 import pl.twojekursy.BaseUnitTest;
+import pl.twojekursy.client.Client;
 import pl.twojekursy.client.ClientService;
+
+import java.util.HashSet;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class AccountantServiceTest extends BaseUnitTest {
 
@@ -31,7 +39,7 @@ class AccountantServiceTest extends BaseUnitTest {
         underTest.create(request);
 
         //then
-        Mockito.verify(accountantRepository).save(accountantCaptor.capture());
+        verify(accountantRepository).save(accountantCaptor.capture());
 
         Accountant accountant = accountantCaptor.getValue();
         assertThat(accountant).isNotNull();
@@ -41,5 +49,62 @@ class AccountantServiceTest extends BaseUnitTest {
         assertThat(accountant.getCreatedDateTime()).isNull();
         assertThat(accountant.getLastModifiedDateTime()).isNull();
         assertThat(accountant.getClients()).isNull();
+    }
+
+    @Test
+    void givenAccountantIdNotExist_whenAttachClient_thenEntityNotFoundException() {
+        // given
+        AttachClientRequest request = new AttachClientRequest(123L, 5463L);
+        when(accountantRepository.findByIdFetchGroupsInfo(request.getAccountantId())).thenReturn(Optional.empty());
+
+        // when
+        Executable executable = () -> underTest.attachClient(request);
+
+        // then
+        Assertions.assertThrows(EntityNotFoundException.class, executable);
+        verifyNoInteractions(clientService);
+    }
+
+    @Test
+    void givenClientIdNotExist_whenAttachClient_thenEntityNotFoundException() {
+        // given
+        AttachClientRequest request = new AttachClientRequest(123L, 5463L);
+        when(accountantRepository.findByIdFetchGroupsInfo(request.getAccountantId()))
+                .thenReturn(Optional.of(mock(Accountant.class)));
+
+        when(clientService.findById(request.getClientId()))
+                .thenThrow(IllegalArgumentException.class);
+
+        // when
+        Executable executable = () -> underTest.attachClient(request);
+
+        // then
+        Assertions.assertThrows(IllegalArgumentException.class, executable);
+    }
+
+    @Test
+    void givenCorrectRequest_whenAttachClient_thenAttachClientToAccountant() {
+        // given
+        AttachClientRequest request = new AttachClientRequest(123L, 5463L);
+        Accountant accountant = Accountant.builder()
+                .id(request.getAccountantId())
+                .clients(new HashSet<>())
+                .build();
+        Client client = Client.builder()
+                .id(request.getClientId())
+                .build();
+
+        when(accountantRepository.findByIdFetchGroupsInfo(request.getAccountantId()))
+                .thenReturn(Optional.of(accountant));
+        when(clientService.findById(request.getClientId()))
+                .thenReturn(client);
+
+        // when
+        underTest.attachClient(request);
+
+        // then
+        assertThat(accountant.getClients())
+                .hasSize(1)
+                .containsExactlyInAnyOrder(client);
     }
 }
